57e73738a89a700e9a007bd197241428645c2200
YSKh616 <hibino616@gmail.com>
2020-06-30 00:22:32 +0900

    83のb

--- ABC/83/b.cpp
+++ ABC/83/b.cpp
@@ -10,18 +10,26 @@ int main()
   int N, A, B;
   cin >> N >> A >> B;
 
+  int ans = 0;
   for (int i = 1; i <= N; i++)
   {
+    int num = i;
+    int sum = 0;
     while (1)
     {
-      if(i % 10 == 0) {
-        
-      } 
+      if(num / 10 == 0) {
+        sum += num;
+        if(A <= sum && sum <= B) {
+          ans += i;
+        }
+        break;
+      } else {
+        sum += num % 10;
+        num = num / 10;
+      }
     }
-    
-    
   }
-  
+  cout << ans << endl;
 
   return 0;
 }
